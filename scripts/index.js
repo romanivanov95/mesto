4a85d5801ec60e7e44856856df39e43e8cd0d5cc
Roman Ivanov <romanivanov1995@yandex.ru>
2022-01-16 13:57:54 +0300

    mesto ver. 1.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,38 @@
 let openPopup = document.querySelector('.profile__edit-button');
 let closePopup = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup');
-let saveForm = document.querySelector('.popup__save-button');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-job');
+let formElement = document.querySelector('.popup__form');
 let profileUserName = document.querySelector('.profile__name');
 let profileUserDescription = document.querySelector('.profile__description');
 /*let likeButton = document.querySelector('.elements__like-button');
-let likeButtonActive = document.querySelector('.elements__like-button_active');*/
+let likeButtonActive = document.querySelector('.elements__like-button_active');
+let saveForm = document.querySelector('.popup__save-button');*/
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
 
-openPopup.addEventListener('click', function() {
+function popupAdd() {
     popup.classList.add('popup_opened');
-})
+    nameInput.value = profileUserName.textContent;
+    jobInput.value = profileUserDescription.textContent;
+}
 
-closePopup.addEventListener('click', function () {
+function popupRemove() {
     popup.classList.remove('popup_opened');
-})
+}
+
+openPopup.addEventListener('click', popupAdd);
+
+closePopup.addEventListener('click', popupRemove);
 
 /*likeButton.addEventListener('click', function() {
     likeButton.classList.add('elements__like-button_active');
 })*/
 
-let formElement = document.querySelector('.popup');
-
 function formSubmitHandler(event) {
     event.preventDefault();
     profileUserName.textContent = nameInput.value;
     profileUserDescription.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
+    popupRemove();
 };
+
 formElement.addEventListener('submit', formSubmitHandler); 
